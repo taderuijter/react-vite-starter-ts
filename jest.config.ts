4d5a602e9068ff5201534a96f030f74fc2d0b4d4
Tadeusz de Ruijter <tech@nomadventrs.com>
2023-02-27 15:23:19 +0100

    feat: Added Jest for testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,17 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   testMatch: ["/src/__tests__/**/*.tsx", "**/?(*.)+(spec|test).tsx"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  snapshotSerializers: ["@emotion/jest/serializer"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-      diagnostics: {
-        warnOnly: true,
-      },
-    },
-  },
 };
 
 export default config;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="pt-4 pb-4 text-center bg-slate-200 mb-4">
+    <div
+      data-testid="navbar"
+      className="pt-4 pb-4 text-center bg-slate-200 mb-4">
       <div className="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0 justify-center">
         <Link
           to="/"
