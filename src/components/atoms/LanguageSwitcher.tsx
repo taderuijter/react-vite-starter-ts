15fac44ba92ed548e3eed508c1026a4ade02b900
Tadeusz de Ruijter <tech@nomadventrs.com>
2023-03-10 00:30:50 +0100

    feat: added flowbite design system

--- src/components/atoms/LanguageSwitcher.tsx
+++ src/components/atoms/LanguageSwitcher.tsx
@@ -1,67 +1,25 @@
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Dropdown } from "flowbite-react";
 
 function LanguageSwitcher(): JSX.Element {
   const { t, i18n } = useTranslation();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = (): void => setIsOpen(!isOpen);
 
   const changeLanguage = (lng: string): void => {
     i18n.changeLanguage(lng);
-    setIsOpen(false);
   };
 
   return (
-    <div className="relative">
-      <button
-        type="button"
-        className="inline-flex justify-center items-center space-x-2 border border-gray-300 rounded-xl px-4 py-3 bg-white text-sm font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        onClick={toggleMenu}
-        aria-haspopup="true"
-        aria-expanded={isOpen ? "true" : "false"}
-        aria-label={t("components.languageSwitcher.label") || ""}>
-        <span>
-          {t("components.languageSwitcher.currentLanguage", {
-            lng: i18n.language,
-          })}
-        </span>
-        <svg
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M19 9l-7 7-7-7"
-          />
-        </svg>
-      </button>
-      {isOpen && (
-        <div
-          className="absolute z-10 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
-          role="menu">
-          <div className="py-1" role="none">
-            <button
-              type="button"
-              className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-              onClick={() => changeLanguage("en")}
-              role="menuitem">
-              {t("components.languageSwitcher.languages.en")}
-            </button>
-            <button
-              type="button"
-              className="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-              onClick={() => changeLanguage("nl")}
-              role="menuitem">
-              {t("components.languageSwitcher.languages.nl")}
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
+    <Dropdown
+      label={t("components.languageSwitcher.currentLanguage", {
+        lng: i18n.language,
+      })}>
+      <Dropdown.Item onClick={() => changeLanguage("en")}>
+        {t("components.languageSwitcher.languages.en")}
+      </Dropdown.Item>
+      <Dropdown.Item onClick={() => changeLanguage("nl")}>
+        {t("components.languageSwitcher.languages.nl")}
+      </Dropdown.Item>
+    </Dropdown>
   );
 }
 
