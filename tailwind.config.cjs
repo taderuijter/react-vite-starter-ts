1efa08812210008ab1009bd680b502470c7275f4
Tadeusz de Ruijter <tech@nomadventrs.com>
2023-03-09 16:53:27 +0100

    feat: added some basic stuff to the Tailwind CSS config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#12022F",
+        secondary: "#594D6D",
+        third: "#D8D5DD",
+        fourth: "#FFDDA9",
+        fifth: "#391484",
+      },
+    },
   },
   plugins: [],
 };
