4c4a3d0ee20d09d0ba27d76b2308593855d7344c
Tadeusz de Ruijter <tech@nomadventrs.com>
2023-03-27 09:07:52 +0200

    feat: added modal component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,8 @@
 import Navbar from "components/layout/Navbar";
+import { Modal } from "components/messages/Modal";
+import { Button } from "components/inputs/Button";
+import { Icon } from "components/labels/Icon";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
 type BannerTypes = {
   children: React.ReactNode;
@@ -6,6 +10,26 @@ type BannerTypes = {
 
 const Layout = ({ children }: BannerTypes) => (
   <>
+    <Modal
+      icon={
+        <Icon
+          icon={<CheckCircleIcon className="h-8 w-8 text-green-400" />}
+          className="bg-green-100"
+        />
+      }
+      title="Sample Modal"
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus odio, pulvinar in convallis a, bibendum eget est."
+      button={
+        <Button
+          type="button"
+          text="Save changes"
+          styling="solid"
+          onClick={() => {
+            console.log("BAMI");
+          }}
+        />
+      }
+    />
     <Navbar />
     {children}
   </>
---
+++ src/components/messages/Modal.tsx
@@ -0,0 +1,70 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Button } from "components/inputs/Button";
+
+// Modal types
+interface ModalTypes {
+  icon: React.ReactNode;
+  title: string;
+  content: string;
+  button: React.ReactNode;
+}
+
+export const Modal = ({ icon, title, content, button }: ModalTypes) => {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0">
+          <div className="fixed inset-0 bg-black bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <div className="sm:flex sm:items-start">
+                  {icon}
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-base font-semibold leading-6 text-gray-900">
+                      {title}
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">{content}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-4 sm:ml-10 sm:flex sm:pl-4 space-x-4">
+                  {button}
+                  <Button
+                    type="button"
+                    styling="outline"
+                    text="Cancel"
+                    onClick={() => setOpen(false)}
+                  />
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
